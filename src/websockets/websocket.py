c532198f86a110e2c56d79d3ce10a339945c90c2
Clever_Moon <qzr19970105@live.com>
2018-12-29 18:12:42 +0800

    💩 需要修正一些代码

--- src/websockets/websocket.py
+++ src/websockets/websocket.py
@@ -55,39 +55,40 @@ class WebSocketConnection(threading.Thread):
         线程启动函数
         :return:
         """
-        wpu = WebSocketProtocol(self.index, self.conn_map)
-        whu = WebSocketHandshake(self.index, self.conn_map)
+        ws_protocol = WebSocketProtocol(self.index, self.conn_map)
+        ws_handshake = WebSocketHandshake(self.index, self.conn_map)
         while True:  # 循环接收WebSocket Client信息
             if self.is_handshake is False:  # WebSocket未建立连接，响应握手请求
-                self.recv_buffer_str = self.conn.recv(1024).decode('utf-8')  # 接收1024字节数据, 并解码
+                self.recv_buffer = self.conn.recv(1024)  # 接收1024字节序列
+                self.recv_buffer_str = self.recv_buffer.decode('utf-8')  # 字节序列解码
                 try:
-                    whu.check_request(self.recv_buffer_str)
+                    ws_handshake.check_request(self.recv_buffer_str)
                 except InvalidFormat as exp:
                     log_debug.logger.error('WebSocket {0}: {1}'.format(self.index, exp.message))
                 except InvalidMultiHeader as exp:
                     log_debug.logger.error('WebSocket {0}: {1}'.format(self.index, exp.message))
                 except InvalidHeader as exp:
                     log_debug.logger.error('WebSocket {0}: {1}'.format(self.index, exp.message))
-                whu.send_response()
-                self.is_online = wpu.heartbeat()
+                ws_handshake.send_response()
+                self.is_online = ws_protocol.heartbeat()
                 if self.is_online is True:  # WebSocket Client响应心跳成功
                     self.is_handshake = True  # WebSocket 连接成功建立之后修改握手标志
                 else:  # WebSocket Client响应心跳失败
-                    wpu.remove_conn()
+                    ws_protocol.remove_conn()
                     break
                 self.recv_buffer_str = ''
             else:  # WebSocket已建立连接，响应控制帧
                 log_debug.logger.info('WebSocket {0} 已连接'.format(self.host))
-                self.recv_buffer = wpu.recv_buffer()
+                self.recv_buffer = ws_protocol.recv_buffer()
                 if self.recv_buffer:  # 数据不为空
                     frame_tuple = WebSocketProtocol.bytify_buffer(self.recv_buffer)  # 解析数据帧字节序列
-                    opcode = wpu.respond_control_frame(frame_tuple)  # 响应控制帧
+                    opcode = ws_protocol.respond_control_frame(frame_tuple)  # 响应控制帧
                     if opcode != 0:
                         log_debug.logger.error('WebSocket {0} {1}号控制帧已响应'.format(self.index, opcode))
                     else:
                         pass
                 else:  # 数据帧为空, WebSocket 异常关闭
-                    wpu.remove_conn()  # 从 WebSocket 连接映射表中删除句柄
+                    ws_protocol.remove_conn()  # 从 WebSocket 连接映射表中删除句柄
                     break
                 self.recv_buffer = b''
                 self.recv_buffer_str = ''
